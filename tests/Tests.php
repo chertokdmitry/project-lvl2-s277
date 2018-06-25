2475c41f5495879249068a0c5cbb70caa9592240
Dmitry Chertok <chertokdmitry@yandex.ru>
2018-06-25 15:36:50 +0300

    Test fix

--- tests/Tests.php
+++ tests/Tests.php
@@ -1,26 +1,20 @@
 <?php
-
 namespace Diff\Tests;
 
-require '../src/Gendiffplain.php';
-require '../src/Parser.php';
-require '../src/Differ.php';
-
-use \Diff\Gendiffplain;
-use \Diff\Parser;
+use \Diff\Gendiff;
 use PHPUnit\Framework\TestCase;
 
-class TestsDiff extends TestCase
+class Tests extends TestCase
 {
-    public function testJson()
+    public function testPretty()
     {
             $file1 = "fixtures/before.json";
             $file2 = "fixtures/after.json";
-            $type = "json";
+            $type = "pretty";
 
-            $result = 'a:5:{i:0;s:17:"  host: hexlet.io";i:1;s:13:"+ timeout: 20";i:2;s:13:"- timeout: 50";i:3;s:22:"- proxy: 123.234.53.22";i:4;s:12:"+ verbose: 1";}';
+            $result= "fixtures/result.txt";
 
-            $diff = \Diff\Gendiffplain\genDiffPlain($file1, $file2, $type);
+            $diff = \Diff\Gendiff\genDiff($file1, $file2, $type);
             $this->assertEquals($diff, $result);
     }
 }
